0283faa5e34097c6a150162ab263c0010fa6e741
jrincon <jrincon@falabella.com>
2024-09-26 11:10:48 -0500

    :sparkles: login con validacion

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,35 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
-
+import { InputTextModule } from 'primeng/inputtext';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    CardModule
+    CardModule,
+    InputTextModule,
+    ReactiveFormsModule,
+    ButtonModule
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['',[Validators.required, Validators.email]],
+      password: ['',Validators.required] // Puedes añadir el campo de contraseña aquí
+    });
+  }
+  onSubmit(){
+
+    if (this.loginForm.valid){
+      const formData = this.loginForm.value;
+      console.log(formData.email+"-"+formData.password);
+    }
+
+  }
 }
