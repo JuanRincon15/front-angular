ad6e5652beb5966aa9b17690784b7c3d4a1be9b4
jrincon <jrincon@falabella.com>
2024-09-25 23:49:39 -0500

    :sparkles: primer commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CardModule } from 'primeng/card';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    CardModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
